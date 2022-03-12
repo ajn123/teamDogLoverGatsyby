4e3defbc61f2bed03aaec662d053167f58a200d6
AlexNorton-ctrl <anorton@saatvamattress.com>
2022-03-12 11:56:06 -0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         },
       },
     },
+    /**
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -39,6 +40,7 @@ module.exports = {
         enableWebVitalsTracking: true,
       },
     },
+    */
 {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
@@ -46,17 +48,17 @@ module.exports = {
         trackingIds: [
           "UA-221551513-1", // Google Analytics / GA
          ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+        // // This object gets passed directly to the gtag config command
+        // // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
